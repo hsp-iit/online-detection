675d277800d590044ebaf833b15494a4e29651fc
elisa maiettini <elisa.maiettini@iit.it>
2020-05-15 13:17:01 +0200

    added Feature extractor abstract class - temporary location

---
+++ src/modules/FeatureExtractorAbstract.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+class FeatureExtractorAbstract(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def loadFeatureExtractor(self) -> None:
+        pass
+
+    @abstractmethod
+    def trainFeatureExtractor(self) -> None:
+        pass
+
+    @abstractmethod
+    def testFeatureExtractor(self) -> None:
+        pass
+
+    @abstractmethod
+    def extractFeatures(self, dataset) -> None:
+        pass
