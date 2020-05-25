2cf40106136e253b3d0ee28fd6c4ec6ea4cea768
elisa maiettini <elisa.maiettini@iit.it>
2020-05-26 00:02:24 +0200

    prepared script for inegration of feat extr and reg class - changed config files to be personal - still need to fix absolute paths in config files

---
+++ experiments/script_RegionClassifier_FeatureExtractor.py
@@ -0,0 +1,66 @@
+import os
+import sys
+
+basedir = os.path.dirname(__file__)
+sys.path.append(os.path.abspath(os.path.join(basedir, '..', '..')))
+sys.path.append(os.path.abspath(os.path.join(basedir, os.path.pardir, 'src', 'modules', 'region-classifier')))
+sys.path.append(os.path.abspath(os.path.join(basedir, os.path.pardir, 'src', 'modules', 'accuracy-evaluator')))
+sys.path.append(os.path.abspath(os.path.join(basedir, '..', '..', 'src', 'modules', 'feature-extractor')))
+
+import OnlineRegionClassifier as ocr
+import FALKONWrapper as falkon
+import MinibootstrapSelector as ms
+import AccuracyEvaluator as ae
+from feature_extractor import FeatureExtractor
+from maskrcnn_pytorch.benchmark.data import make_data_loader
+from maskrcnn_pytorch.benchmark.config import cfg
+
+
+# Temporary imports
+import torch
+# Experiment configuration
+cfg.merge_from_file('/home/elisa/Repos/python-online-detection/experiments/Configs/first_experiment_elisa.yaml')
+cfg.freeze()
+dataset = make_data_loader(cfg, is_train=False, is_distributed=False, is_target_task=True)
+cfg_path = '/home/elisa/Repos/python-online-detection/experiments/Configs/config_region_classifier_elisa.yaml'
+classifier = falkon.FALKONWrapper()
+negative_selector = ms.MinibootstrapSelector(cfg_path)
+regionClassifier = ocr.OnlineRegionClassifier(classifier, negative_selector, cfg_path=cfg_path)
+accuracy_evaluator = ae.AccuracyEvaluator(cfg_path)
+feature_extractor = FeatureExtractor('configs/config_feature_task_elisa.yaml', 'configs/config_target_task_FALKON_elisa.yaml')
+
+# Retrieve feature extractor (either by loading it or by training it)
+try:
+    feature_extractor.loadFeatureExtractor()
+except OSError:
+    print('Feature extractor will be trained from scratch.')
+    feature_extractor.trainFeatureExtractor()
+
+# Extract features for the train/val/test sets
+feature_extractor.extractFeatures()
+
+# Train region refiner
+print('Skip region refiner training')
+
+# Start the cross validation
+print('Skip cross validation')
+
+# - Train region classifier
+model = regionClassifier.trainRegionClassifier()
+# model = torch.load('model_icub_test_TASK2_30objs_manual')
+# - Test region classifier (on validation set)
+print('Skip Test region classifier on validation set')
+
+# - Test region refiner (on validation set)
+print('Skip Test region refiner on validation set')
+
+# - Save/store results
+print('Skip saving model')
+
+# Test the best model (on the test set)
+predictions = regionClassifier.testRegionClassifier(model)
+
+result_cls = accuracy_evaluator.evaluate(dataset.dataset, predictions, is_target_task=True, cls_agnostic_bbox_reg=True)
+
+# Test region refiner (on test set)
+print('Skip Test region refiner on test set')
