072dc2fb6f5dddafc19ab6d555b9264ae934a618
fedeceola <federico.ceola@iit.it>
2021-03-30 17:45:32 +0200

    Added train for time option for full training o mask rcnn

--- experiments/run_experiment_full_train.py
+++ experiments/run_experiment_full_train.py
@@ -12,6 +12,8 @@ from feature_extractor import FeatureExtractor
 parser = argparse.ArgumentParser()
 parser.add_argument('--output_dir', action='store', type=str, default='full_train_ycbv', help='Set experiment\'s output directory.')
 parser.add_argument('--config_file', action='store', type=str, default="configs/config_full_train_ycbv.yaml", help='Manually set configuration file, by default it is configs/config_full_train_ycbv.yaml. If the specified path is not absolute, the config file will be searched in the experiments directory')
+parser.add_argument('--train_for_time', action='store', type=str, help='Train mask for the input training time. It must be in the format XXh:YYm:ZZs.')
+
 
 args = parser.parse_args()
 
@@ -30,7 +32,33 @@ if args.config_file.startswith("/"):
 else:
     cfg_feature_task = os.path.abspath(os.path.join(basedir, args.config_file))
 
+training_seconds=None
+if args.train_for_time:
+    for i in range(len(args.train_for_time)):
+        if (i == 0 or i ==1 or i ==4 or i==5 or i==8 or i ==9):
+            if not args.train_for_time[i].isdigit():
+                print('The training time format must be XXh:YYm:ZZs.')
+                quit()
+        if (i == 3 or i ==7):
+            if not args.train_for_time[i] == ':':
+                print('The training time format must be XXh:YYm:ZZs.')
+                quit()
+        if i == 2 and not args.train_for_time[i] == 'h':
+            print('The training time format must be XXh:YYm:ZZs.')
+            quit()
+        if i == 6 and not args.train_for_time[i] == 'm':
+            print('The training time format must be XXh:YYm:ZZs.')
+            quit()
+        if i == 10 and not args.train_for_time[i] == 's':
+            print('The training time format must be XXh:YYm:ZZs.')
+            quit()
+    seconds = 10*int(args.train_for_time[8]) + int(args.train_for_time[9])
+    minutes = 10*int(args.train_for_time[4]) + int(args.train_for_time[5])
+    hours = 10*int(args.train_for_time[0]) + int(args.train_for_time[1])
+
+    training_seconds = seconds + 60*minutes + 3600*hours
+
 # Initialize feature extractor
 feature_extractor = FeatureExtractor(cfg_path_feature_task=cfg_feature_task)
 
-feature_extractor.trainFeatureExtractor(output_dir=output_dir)
+feature_extractor.trainFeatureExtractor(output_dir=output_dir, training_seconds=training_seconds)
